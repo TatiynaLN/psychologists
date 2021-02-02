b91e9e99f4178bb81ac83f6b8d78a27bd3af82e5
TatiynaLN <tania.cis2011@yandex.ru>
2021-02-03 00:06:20 +0300

    add

--- src/js/custom/body.js
+++ src/js/custom/body.js
@@ -4,9 +4,10 @@
     //     $('.form').click(function () {
     //         $('body').toggleClass('oh');
     //     });
-
-
-
+    let panel = document.querySelector('.body');
+    document.querySelector('.menu__btn').addEventListener('click', e => {
+        panel.classList.toggle('oh');
+    });
 })();
 "use strict";
 ;
