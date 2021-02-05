7fb6cbe4315d4f9df988dc2819804c320befda19
TatiynaLN <tania.cis2011@yandex.ru>
2021-02-05 13:36:07 +0300

    add

--- src/js/custom/slider.js
+++ src/js/custom/slider.js
@@ -14,7 +14,44 @@
         //активный слайд по центру
         // centerredSlides: true,
         //стартовый слайд отсчет с 0
-        initialSlide: 1,
+        initialSlide: 0,
+        autoHeight: true,
+        loop: true,
+        breakpoints: {
+            320: {
+                //колличество слайдов для показа
+                slidesPerView: 1,
+            },
+            768: {
+                //колличество слайдов для показа
+                slidesPerView: 2,
+            }
+
+        },
+    });
+
+})();
+"use strict";
+;
+// "use strict";
+// ;
+
+(function () {
+    new Swiper('.education-sliders', {
+        //стрелки
+        navigation: {
+            nextEl: '.swiper-buton-lefts',
+            prevEl: '.swiper-buton-rights'
+        },
+
+        //колличество пролистываемых слайдов
+        slidesPerGroup: 1,
+        //активный слайд по центру
+        // centerredSlides: true,
+        //стартовый слайд отсчет с 0
+        initialSlide: 0,
+        autoHeight: true,
+        loop: true, //бесконечная прокрутка
         breakpoints: {
             320: {
                 //колличество слайдов для показа
@@ -47,6 +84,8 @@
         initialSlide: 1,
         spaceBetween: 60,
         centeredSlides: true,
+        autoHeight: true,
+        loop: true,
         breakpoints: {
             320: {
                 //колличество слайдов для показа
