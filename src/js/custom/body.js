1891deb0b79a5fc06d762d767d85ecb5993da077
TatiynaLN <tania.cis2011@yandex.ru>
2021-02-08 19:40:43 +0300

    add

--- src/js/custom/body.js
+++ src/js/custom/body.js
@@ -40,3 +40,55 @@
 })();
 "use strict";
 ;
+// (function () {
+//     var body = document.getElementsByTagName('body')[0]; // Получаем боди
+
+//     var html = body.parentNode; // Получаем html
+
+//     var modalFirst = document.querySelector('.modalDialog');
+//     var modalSecond = document.querySelector('.modalDialogTwo');
+//     var modalThird = document.querySelector('.modalfooter');
+//     document.addEventListener('click', function (e) {
+
+//         if (e.target.classList.contains('modal-first')) {
+
+
+//             modalFirst.classList.add('modal--open'); // Добавляем по клику класс, по которому модалка, которых две, откроется (его надо прописать в css)
+
+//             body.classList.add('oh'); // Запрет прокрутки боди
+
+//             html.classList.add('html-overflow'); // Предотвращение дергания экрана при открытии модалки
+//         } else if (e.target.classList.contains('btn_left')) {
+//             modalSecond.classList.add('modal--open'); // Добавляем по клику класс, по которому модалка, которая одна по середине, откроется (его надо прописать в css)
+
+//             body.classList.add('oh'); // Запрет прокрутки боди
+
+//             html.classList.add('html-overflow'); // Предотвращение дергания экрана при открытии модалки
+//         } else if (e.target.classList.contains('btn__footer-placement')) {
+//             modalThird.classList.add('modal--open'); // Добавляем по клику класс, по которому модалка, которая одна по середине, откроется (его надо прописать в css)
+
+//             body.classList.add('oh'); // Запрет прокрутки боди
+
+//             html.classList.add('html-overflow'); // Предотвращение дергания экрана при открытии модалки
+//         } else if (e.target.classList.contains('close') && modalFirst.classList.contains('modal--open')) {
+//             // Если клик был по крестику и первая или нижняя модалка открыты, то они закрываются
+//             body.classList.remove('oh'); // Снятие запрета прокрутки боди
+
+//             html.classList.remove('html-overflow'); // Снятие блокировки html
+
+//             modalFirst.classList.remove('modal--open');
+//         } else if (e.target.classList.contains('close') && modalSecond.classList.contains('modal--open')) {
+//             // Если клик был по крестику и средняя модалка открыта, то она закрывается
+//             modalSecond.classList.remove('modal--open');
+//             body.classList.remove('oh'); // Снятие запрета прокрутки боди
+
+//             html.classList.remove('html-overflow'); // Снятие блокировки html
+//         } else if (e.target.classList.contains('close') && modalThird.classList.contains('modal--open')) {
+//             // Если клик был по крестику и средняя модалка открыта, то она закрывается
+//             modalThird.classList.remove('modal--open');
+//             body.classList.remove('oh'); // Снятие запрета прокрутки боди
+
+//             html.classList.remove('html-overflow'); // Снятие блокировки html
+//         }
+//     });
+// })();
