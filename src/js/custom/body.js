498b1a0dece40292c7e7db2b1600d04a242c350e
TatiynaLN <tania.cis2011@yandex.ru>
2021-02-16 10:39:31 +0300

    add

--- src/js/custom/body.js
+++ src/js/custom/body.js
@@ -1,9 +1,6 @@
 "use strict";
 ;
 (function () {
-    //     $('.form').click(function () {
-    //         $('body').toggleClass('oh');
-    //     });
     let panel = document.querySelector('.bo');
     document.querySelector('.menu__btn').addEventListener('click', e => {
         panel.classList.toggle('oh');
@@ -13,82 +10,27 @@
 ;
 (function () {
 
-    // const body = document.getElementsByTagName('body')[0];
-    // const html = body.parentNode;
-    // const testModal = document.querySelectorAll('.btn');
-    // testModal.forEach(btn => {
-    //     btn.addEventListener('click', () => {
+    let map_container = document.getElementById('map_container');
+    let options_map = {
+        once: true,
+        passive: true,
+        capture: true
+    };
+    map_container.addEventListener('click', start_lazy_map, options_map);
+    map_container.addEventListener('mouseover', start_lazy_map, options_map);
+    map_container.addEventListener('touchstart', start_lazy_map, options_map);
+    map_container.addEventListener('touchmove', start_lazy_map, options_map);
 
-    //     });
-    // });
-    // document.addEventListener("DOMContentLoaded", function () {
-    //     var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
-    //     console.log(scrollbar);
-    //     document.querySelector('[href="#openModal"]').addEventListener('click', function () {
-    //         document.body.style.overflow = 'hidden';
-    //         document.querySelector('#openModal').style.marginLeft = scrollbar;
-    //     });
-    //     document.querySelector('[href="#close"]').addEventListener('click', function () {
-    //         document.body.style.overflow = 'visible';
-    //         document.querySelector('#openModal').style.marginLeft = '0px';
-    //     });
-    // });
+    let map_loaded = false;
+    function start_lazy_map() {
+        if (!map_loaded) {
+            let map_block = document.getElementById('ymap_lazy');
+            map_loaded = true;
+            map_block.setAttribute('src', map_block.getAttribute('data-src'));
+            map_block.removeAttribute('data-src');
+            console.log('YMAP LOADED');
+        }
+    }
 })();
 "use strict";
 ;
-(function () {
-})();
-"use strict";
-;
-// (function () {
-//     var body = document.getElementsByTagName('body')[0]; // Получаем боди
-
-//     var html = body.parentNode; // Получаем html
-
-//     var modalFirst = document.querySelector('.modalDialog');
-//     var modalSecond = document.querySelector('.modalDialogTwo');
-//     var modalThird = document.querySelector('.modalfooter');
-//     document.addEventListener('click', function (e) {
-
-//         if (e.target.classList.contains('modal-first')) {
-
-
-//             modalFirst.classList.add('modal--open'); // Добавляем по клику класс, по которому модалка, которых две, откроется (его надо прописать в css)
-
-//             body.classList.add('oh'); // Запрет прокрутки боди
-
-//             html.classList.add('html-overflow'); // Предотвращение дергания экрана при открытии модалки
-//         } else if (e.target.classList.contains('btn_left')) {
-//             modalSecond.classList.add('modal--open'); // Добавляем по клику класс, по которому модалка, которая одна по середине, откроется (его надо прописать в css)
-
-//             body.classList.add('oh'); // Запрет прокрутки боди
-
-//             html.classList.add('html-overflow'); // Предотвращение дергания экрана при открытии модалки
-//         } else if (e.target.classList.contains('btn__footer-placement')) {
-//             modalThird.classList.add('modal--open'); // Добавляем по клику класс, по которому модалка, которая одна по середине, откроется (его надо прописать в css)
-
-//             body.classList.add('oh'); // Запрет прокрутки боди
-
-//             html.classList.add('html-overflow'); // Предотвращение дергания экрана при открытии модалки
-//         } else if (e.target.classList.contains('close') && modalFirst.classList.contains('modal--open')) {
-//             // Если клик был по крестику и первая или нижняя модалка открыты, то они закрываются
-//             body.classList.remove('oh'); // Снятие запрета прокрутки боди
-
-//             html.classList.remove('html-overflow'); // Снятие блокировки html
-
-//             modalFirst.classList.remove('modal--open');
-//         } else if (e.target.classList.contains('close') && modalSecond.classList.contains('modal--open')) {
-//             // Если клик был по крестику и средняя модалка открыта, то она закрывается
-//             modalSecond.classList.remove('modal--open');
-//             body.classList.remove('oh'); // Снятие запрета прокрутки боди
-
-//             html.classList.remove('html-overflow'); // Снятие блокировки html
-//         } else if (e.target.classList.contains('close') && modalThird.classList.contains('modal--open')) {
-//             // Если клик был по крестику и средняя модалка открыта, то она закрывается
-//             modalThird.classList.remove('modal--open');
-//             body.classList.remove('oh'); // Снятие запрета прокрутки боди
-
-//             html.classList.remove('html-overflow'); // Снятие блокировки html
-//         }
-//     });
-// })();
